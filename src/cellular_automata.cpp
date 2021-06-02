b8299849e030afe9494636e263ec4c7f1ca6cf98
t3rmit4nk <official.simone.rizzo@gmail.com>
2021-06-03 00:47:05 +0200

    boooooooooooooooooo

--- src/cellular_automata.cpp
+++ src/cellular_automata.cpp
@@ -4,14 +4,13 @@
 #include <functional>
 #include <vector>
 #include <thread>
+#include <cmath>
 
 using namespace std;
 
 struct range{
     pair<int, int> start;
     pair<int, int> end;
-
-
 };
 
 template<class T>
@@ -23,19 +22,42 @@ class CellularAutomata{
     function<T(T, T[])> _rule;
     size_t _parallelism;
     size_t _nIterations;
-    bool emitterEnabled;
+    //bool emitterEnabled;
     
-    thread _emitter;
+    //thread _emitter;
     vector<thread> _workers;
     vector<range> _ranges;
     //TODO blocking queue
 
     void init ()  {
         //initEmitter();
+        initRanges();
         initWorkers();
     }
 
-    void initEmitter(){
+    void initRanges(){
+        auto size = _n * _m;
+        int workLoad = ceil(double(size) / double(_parallelism));
+        pair<int, int> wl_blocco = make_pair(workLoad / _m, workLoad % _m);
+        pair<int,int> index = make_pair(0,0);
+        for (int k = 0; k < _parallelism; k++) {
+            int i = index.first;
+            int j = index.second;
+            int new_j = (j - 1 + wl_blocco.second) % _m;
+            int riporto = (j + wl_blocco.second) / _m;
+            int new_i = (i + riporto + wl_blocco.first);
+            if (new_i >= _n) {
+                new_i = _n - 1;
+                new_j = _m - 1;
+            }
+            cout << i << " " << j << endl;
+            cout << new_i << " " << new_j << endl;
+            index.first =  new_i;
+            index.second = (new_j+1)%_m;
+        }
+    }
+
+    /*void initEmitter(){
         _emitter = thread([&]{
             for(int i=0; i<_nIterations; i++){
                 _ranges=assignRanges();
@@ -44,7 +66,7 @@ class CellularAutomata{
                 //write
             }
         });
-    }
+    }*/
 
     void initWorkers(){
         
@@ -53,6 +75,7 @@ class CellularAutomata{
                 T currState=getState();
                 T[] neighbors=getNeighbors();
                 _rule(neighbors);
+                //barrier 
             }
         })
         
@@ -64,7 +87,7 @@ class CellularAutomata{
 
 
     public:
-    T** cellularAutomata(size_t n, size_t m, function<T(T, T[])> rule, T** initialState, size_t nIterations, size_t parallelism) : _workers(parallelism){
+    T** cellularAutomata(size_t n, size_t m, function<T(T, T[])> rule, T** initialState, size_t nIterations, size_t parallelism){
         _rule=rule;
         _n=n;
         _m=m;
@@ -72,6 +95,9 @@ class CellularAutomata{
         //TODO: if less than 2 error
         _parallelism=parallelism;
         _nIterations=nIterations;
+        _workers();
+        _ranges();
+        init();
     }
 
 };
