3be3faba4f13b2e66cb54a89bd7a945d8589fdde
Simone <official.simone.rizzo@gmail.com>
2021-06-04 00:16:43 +0200

    Barriera funzionante

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <iostream>
 #include <stdlib.h>
-#include <cmath>
+#include <tuple>
 #include <vector>
+#include <atomic>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <chrono>
+
+
 using namespace std;
 
 struct range{
@@ -11,9 +18,32 @@ struct range{
 };
 
 int main(){
-    vector<vector<int>> prova(8, vector<int>(3, 0));
-    
-    cout<<prova[0][0]<<endl;
+    mutex b1Mutex;
+    int b1 = 0;
+    condition_variable b1Condition;
+    int _parallelism = 5;
+    vector<thread> threads(_parallelism);
+    for (int i = 0; i < 5; i++) {
+        threads[i] = thread([i,&b1, &b1Mutex, &_parallelism, &b1Condition]() {
+            int wait_time = rand() % 10 + 1;
+            cout << "thread:" << i << " attende: " << wait_time << endl;
+            this_thread::sleep_for(std::chrono::seconds(wait_time));
+            //fine della computazione
+            unique_lock<mutex> lock1(b1Mutex);
+            //counter increment
+            b1++;
+            b1Condition.wait(lock1, [&]() {
+                return !(b1 > 0 && b1 < _parallelism);
+            });
+            cout << "thread:" << i << " mi hanno svegliato: " << wait_time << endl;
+            if (b1 == _parallelism) {
+                cout << "Sono l'ultimo thread: " << i << endl;
+                b1 = 0;
+                b1Condition.notify_all();
+            }
+            lock1.release();
+            });
+    }
     return 0;
 
 }
