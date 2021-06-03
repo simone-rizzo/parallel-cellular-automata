37ba913698b55cbe9ae4110f8d06ca0f2b600bd0
t3rmit4nk <official.simone.rizzo@gmail.com>
2021-06-03 21:47:16 +0200

    +++

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <iostream>
 #include <stdlib.h>
-
+#include <cmath>
+#include <vector>
 using namespace std;
 
-int main(){
+struct range{
+    pair<int, int> start;
+    pair<int, int> end;
+};
 
-    cout<<"ciao"<<endl;
+int main(){
+    vector<vector<int>> prova(8, vector<int>(3, 0));
+    
+    cout<<prova[0][0]<<endl;
     return 0;
 
 }
