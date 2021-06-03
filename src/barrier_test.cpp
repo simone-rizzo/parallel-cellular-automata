ca41026d52b7d2e1a1c527df88d115e80239a0fd
Simone <official.simone.rizzo@gmail.com>
2021-06-04 00:16:43 +0200

    Barriera funzionante

---
+++ src/barrier_test.cpp
@@ -0,0 +1,70 @@
+// ProvaCpiupiu.cpp : Questo file contiene la funzione 'main', in cui inizia e termina l'esecuzione del programma.
+//
+
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+#include <tuple>
+#include <vector>
+#include <atomic>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <chrono>
+
+
+using namespace std;
+int main() {
+    mutex b1Mutex;
+    int b1 = 0;
+    condition_variable b1Condition;
+
+    mutex b2Mutex;
+    int b2 = 0;
+    condition_variable b2Condition;
+
+    int _parallelism = 5;
+    vector<thread> threads(_parallelism);
+    for (int i = 0; i < 5; i++) {
+        threads[i] = thread([i,&b1, &b1Mutex,&b2,&b2Mutex, &_parallelism, &b1Condition, &b2Condition]() {
+            srand(i);
+            int wait_time = rand() % 10 + 1;
+            cout << "thread:" << i << " attende: " << wait_time << endl;
+            this_thread::sleep_for(std::chrono::seconds(wait_time));
+            //fine della computazione
+            {
+                unique_lock<mutex> lock1(b1Mutex);
+                //counter increment
+                b1++;
+                b1Condition.wait(lock1, [&]() {
+                    return !(b1 != 0 && b1 != _parallelism);
+                });
+                if (b1 == _parallelism) {
+                    cout << "Sono l'ultimo thread: " << i << endl;
+                    b1 = 0;
+                    b1Condition.notify_all();
+                }
+                cout << "thread:" << i << " mi hanno svegliato: " << wait_time << endl;    
+            }
+            this_thread::sleep_for(std::chrono::seconds(wait_time));
+            {
+                unique_lock<mutex> lock2(b2Mutex);
+                //counter increment
+                b2++;
+                b2Condition.wait(lock2, [&]() {
+                    return !(b2 != 0 && b2 != _parallelism);
+                    });
+                if (b2 == _parallelism) {
+                    cout << "Sono l'ultimo thread: " << i << endl;
+                    b2 = 0;
+                    b2Condition.notify_all();
+                }
+                cout << "thread:" << i << " mi hanno svegliato: " << wait_time << endl;
+            }
+            });
+    }
+    for(int i=0;i<_parallelism;i++)
+        threads[i].join();
+    
+    return 0;
+}
